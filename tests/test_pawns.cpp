5966e47036ba6fa98c023660d73d31401d9b92d5
Calileus <79671075+Calileus@users.noreply.github.com>
2026-02-24 15:26:07 -0600

    Create tests/test_pawns.cpp file

---
+++ tests/test_pawns.cpp
@@ -0,0 +1,14 @@
+/// @file      test_pawns.cpp
+/// @brief     Unit tests for the Pawn class using Google Test framework.
+/// @author    Calileus
+/// @date      2026-02-24
+/// @copyright 2026 Obsidian Honor Coders. Licensed under Apache 2.0.
+/// @see       https://github.com/ObsidianHonorCoders/inheritance-chess
+/// @details   Test suite for Pawn class functionality.
+/// @note      Uses std::unique_ptr for automatic memory management
+///            following modern C++ RAII principles.
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#include "pawns.hpp"
